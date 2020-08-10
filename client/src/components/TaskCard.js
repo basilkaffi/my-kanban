f8d6fd864cf05351aca5e87d8c209c730ed93a64
basilkaffi <basilarr7@gmail.com>
2020-08-10 20:29:08 +0700

    navbar app page

--- client/src/components/TaskCard.js
+++ client/src/components/TaskCard.js
@@ -1,28 +1,44 @@
-import React from 'react';
-import deleteIcon from '../assets/delete.png';
-import nextIcon from '../assets/next.png';
+import React from "react";
+import { useState } from "react";
+import deleteIcon from "../assets/delete.png";
+import nextIcon from "../assets/next.png";
+import editIcon from "../assets/edit.png";
+import close from "../assets/close.png";
 
 export default function TaskCard(props) {
-  const task = props.task
-  const category = props.category
+  const [edit, setEdit] = useState(false);
+  const task = props.task;
+  const category = props.category;
   const name = () => {
-    return task.User.name.toUpperCase()
-  }
+    return task.User.name.toUpperCase();
+  };
   const date = () => {
-    return task.createdAt.slice(0,10)
-  }
+    return task.createdAt.slice(0, 10);
+  };
 
-  return (
+  return !edit ? (
     <div className="cardSize taskCard">
-      <div className="title">{task.title}</div>
       <div className="detail">{name()}</div>
       <div className="detail">{date()}</div>
-      {category !== 'completed' ? 
-      <div className="button-container position">
-        <img src={deleteIcon} alt={"delete icon"} />
-        <img src={nextIcon} alt={"next icon"} />
-      </div> : 
-      <img className="position" src={deleteIcon} alt={"delete icon"} />}
+      <div className="title">{task.title}</div>
+      {category !== "completed" ? (
+        <div className="button-container">
+          <img src={deleteIcon} alt={"delete icon"} />
+          <img src={editIcon} alt={"edit icon"} onClick={() => setEdit(true)} />
+          <img src={nextIcon} alt={"next icon"} />
+        </div>
+      ) : (
+        <div className="button-container dua">
+          <img src={deleteIcon} alt={"delete icon"} />
+          <img src={editIcon} alt={"edit icon"} onClick={() => setEdit(true)} />
+        </div>
+      )}
     </div>
-  )
+  ) : (
+    <div className="cardSize addForm">
+      <img src={close} onClick={() => setEdit(false)} />
+      <input type="text" value={task.title} />
+      <input className="addButton" type="submit" value="UPDATE" />
+    </div>
+  );
 }
--- client/src/pages/AppPage.js
+++ client/src/pages/AppPage.js
@@ -1,21 +1,37 @@
-import React from 'react';
-import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import { getItem } from '../store/actions/crudAction';
-import CategoryBar from '../components/CategoryBar';
+import React from "react";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { getItem } from "../store/actions/crudAction";
+import CategoryBar from "../components/CategoryBar";
+import logo from "../assets/kanban.png";
+import { Button } from 'react-bootstrap';
 
 function AppPage() {
-  const dispatch = useDispatch()
-  const categories = ['backlog', 'todo', 'done', 'completed']
+  const dispatch = useDispatch();
+  const categories = ["backlog", "todo", "done", "completed"];
 
   useEffect(() => {
-    dispatch(getItem())
-  }, [dispatch])
+    dispatch(getItem());
+  }, [dispatch]);
 
   return (
     <div className="AppPage background">
+      <div className="header">
+        <div className="logo">
+          <img src={logo} alt={"logo website"} />
+          <div className="text">MY-KANBAN</div>
+        </div>
+        <div className="dropdown">
+          <div className="username">{localStorage.username.toUpperCase()}</div>
+          <div className="dropdownContent">
+            <Button className="logout" >Logout</Button>
+          </div>
+        </div>
+      </div>
       <div className="divider">
-        {categories.map(category => <CategoryBar category ={category} />)}
+        {categories.map((category) => (
+          <CategoryBar category={category} />
+        ))}
       </div>
     </div>
   );
