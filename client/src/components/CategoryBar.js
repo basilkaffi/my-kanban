96ad54009ac51360981f14c918415566d331b39a
basilkaffi <basilarr7@gmail.com>
2020-08-10 13:58:04 +0700

    update card button

--- client/src/components/CategoryBar.js
+++ client/src/components/CategoryBar.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import { useState } from 'react';
-import { useSelector } from 'react-redux';
-import TaskCard from './TaskCard';
-import addIcon from '../assets/plus.png';
-import close from '../assets/close.png'
+import React from "react";
+import { useState } from "react";
+import { useSelector } from "react-redux";
+import TaskCard from "./TaskCard";
+import addIcon from "../assets/plus.png";
+import close from "../assets/close.png";
 
 export default function CategoryBar(props) {
-  const category = props.category
-  const tasks = useSelector(state => state.crudReducer.items)
-  const [showForm, setShow] = useState(false)
-  
+  const category = props.category;
+  const tasks = useSelector((state) => state.crudReducer.items);
+  const [showForm, setShow] = useState(false);
+
   return (
     <div className="categoryBar">
       <div className="header">
@@ -17,16 +17,19 @@ export default function CategoryBar(props) {
         <img src={addIcon} alt={"add icon"} onClick={() => setShow(true)} />
       </div>
       <div className="card-container">
-        {
-          showForm ? <div className="cardSize addForm">
+        {showForm ? (
+          <div className="cardSize addForm">
             <img src={close} onClick={() => setShow(false)} />
             <input type="text" placeholder="Insert Task Title..." />
             <input className="addButton" type="submit" value="ADD" />
-          </div> : null}
-        {
-          tasks.map(task => task.category === category ? <TaskCard task = {task}/>: null
-        ) }
+          </div>
+        ) : null}
+        {tasks.map((task) =>
+          task.category === category ? (
+            <TaskCard category={category} task={task} />
+          ) : null
+        )}
       </div>
     </div>
-  )
+  );
 }
--- client/src/components/TaskCard.js
+++ client/src/components/TaskCard.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import deleteIcon from '../assets/delete.png';
+import nextIcon from '../assets/next.png';
 
 export default function TaskCard(props) {
   const task = props.task
+  const category = props.category
   const name = () => {
     return task.User.name.toUpperCase()
   }
@@ -14,6 +17,12 @@ export default function TaskCard(props) {
       <div className="title">{task.title}</div>
       <div className="detail">{name()}</div>
       <div className="detail">{date()}</div>
+      {category !== 'completed' ? 
+      <div className="button-container position">
+        <img src={deleteIcon} alt={"delete icon"} />
+        <img src={nextIcon} alt={"next icon"} />
+      </div> : 
+      <img className="position" src={deleteIcon} alt={"delete icon"} />}
     </div>
   )
 }
