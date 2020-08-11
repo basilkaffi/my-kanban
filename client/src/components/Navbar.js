cc13b125f714c1b8047bbaae1191920a1dcf0ed5
basilkaffi <basilarr7@gmail.com>
2020-08-11 15:14:18 +0700

    call google signOut in logout

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -2,12 +2,16 @@ import React from 'react';
 import logo from "../assets/kanban.png";
 import { Button } from 'react-bootstrap';
 import { useHistory } from 'react-router-dom';
-import { GoogleLogout } from 'react-google-login';
+import { useGoogleLogout } from 'react-google-login';
 
 function Navbar() {
   const history = useHistory()
+  const { signOut } = useGoogleLogout({
+    clientId: "1042957079851-vfdt9uj25elvk11q6h5foh4k1o687sq1.apps.googleusercontent.com"
+  })
   const logout = () => {
     localStorage.clear()
+    signOut()
     history.push('/')
   }
   return(
@@ -19,19 +23,7 @@ function Navbar() {
       <div className="dropdown">
         <div className="username">{localStorage.username.toUpperCase()}</div>
         <div className="dropdownContent">
-          {/* <Button className="logout" onClick={logout} >Logout</Button> */}
-          <GoogleLogout
-          clientId="1042957079851-vfdt9uj25elvk11q6h5foh4k1o687sq1.apps.googleusercontent.com"render={(renderProps) => (
-            <Button
-              onClick={renderProps.onClick}
-              disabled={renderProps.disabled}
-              className="logout"
-            >
-              Logout
-            </Button>
-          )}
-          onLogoutSuccess={logout}
-          ></GoogleLogout>
+          <Button className="logout" onClick={logout} >Logout</Button>
         </div>
       </div>
     </div>
